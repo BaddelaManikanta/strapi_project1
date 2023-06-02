1bcc2fdb52afb73ff4428c99fff83d8106831aa4
BaddelaManikanta <baddelamanikanta@gmail.com>
2023-06-02 16:36:08 +0530

    message: project updated..

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -6,4 +6,22 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post', ({ strapi }) => ({
+    async exampleAction(ctx) {
+      try {
+        ctx.body = "ok";
+      } catch (err) {
+        ctx.body = err;
+      }
+    },
+    async find(ctx){
+      const {data,meta}= await super.find(ctx);
+      if(ctx.state.user) return {data,meta};
+      const filterData= data.filter((post)=>!post.attributes.premium);
+      return {data:filterData, meta}
+    },
+    async likePost(ctx){
+        if(!ctx.state.user) return ctx.forbidden("Only Authenticated peoples likes posts")
+        
+    }
+  }));
